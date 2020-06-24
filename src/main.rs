83aa8608993e866ce08d10f43b5a155746f665bd
Grant Horner <h.grant.horner@gmail.com>
2020-06-24 11:52:27 -0400

    Load settings from file

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,51 @@
 mod models;
 
+use models::{CacheRecord, Settings};
+use ron::ser::{to_string_pretty, PrettyConfig};
 use std::fs::{create_dir, File};
 use std::io::Write;
+use std::path::PathBuf;
 
 fn main() -> std::io::Result<()> {
     let bookmark_config_path = dirs::home_dir()
         .expect("Could not find home directory.")
         .join(".bkmk_config");
     let folder_exists = bookmark_config_path.exists();
+
     if !folder_exists {
-        let settings = models::Settings::new();
-        create_dir(&bookmark_config_path).expect("Could not create a config directory.");
-        File::create(&bookmark_config_path.join("cache.ron"))
-            .expect("Could not create a cache file.");
-        let mut settings_file = File::create(&bookmark_config_path.join("settings.ron"))
-            .expect("Could not create a cache file.");
-        settings_file
-            .write(settings.serialize().as_bytes())
-            .expect("Could not write to file.");
+        initialize_files(&bookmark_config_path);
     }
-    println!("{:?}", bookmark_config_path);
-    println!("{}", folder_exists);
+
+    let mut settings_file = File::open(bookmark_config_path.join("settings.ron"))
+        .expect("Could not load settings file.");
+
+    let settings = Settings::load(&mut settings_file).expect("Could not load settings file.");
+
+    println!("Could find folder? {}", folder_exists);
+    println!("{:?}", settings);
 
     Ok(())
+}
+
+fn initialize_files(path: &PathBuf) {
+    create_dir(path).expect("Could not create a config directory.");
+    let mut cache_file =
+        File::create(path.join("cache.ron")).expect("Could not create a cache file.");
+    let cache: Vec<CacheRecord> = Vec::new();
+    let pretty = PrettyConfig::new()
+        .with_depth_limit(2)
+        .with_separate_tuple_members(true)
+        .with_enumerate_arrays(true);
+    let data = to_string_pretty(&cache, pretty).expect("Serialization failed.");
+
+    cache_file
+        .write(data.as_bytes())
+        .expect("Could not write to cache file.");
+
+    let settings = models::Settings::new();
+    let mut settings_file =
+        File::create(path.join("settings.ron")).expect("Could not create a cache file.");
+    settings_file
+        .write(settings.serialize().as_bytes())
+        .expect("Could not write to settings file.");
 }
--- src/models.rs
+++ src/models.rs
@@ -1,15 +1,27 @@
+use core::str::FromStr;
 use regex::Regex;
 use ron::ser::{to_string_pretty, PrettyConfig};
-use serde::Serialize;
-use std::convert::TryFrom;
+use serde::{Deserialize, Serialize};
+use std::convert::{From, TryFrom};
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
 
-#[derive(Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
+pub struct CacheRecord {
+    pub name: String,
+    pub description: String,
+    pub path: PathBuf,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
 pub enum KeyBinds {
     Default,
     Emacs,
     Vim,
 }
 
+#[derive(Debug)]
 pub struct Settings {
     pub ignore: Vec<Regex>,
     pub key_binds: KeyBinds,
@@ -28,26 +40,45 @@ impl Settings {
             .with_depth_limit(2)
             .with_separate_tuple_members(true)
             .with_enumerate_arrays(true);
-        let ser = SerializableSettings::try_from(self)
-            .expect("Failed to convert settings to serializable form.");
-        println!("{:?}", ser);
+        let ser = SerializableSettings::from(self);
         to_string_pretty(&ser, pretty).expect("Serialization failed.")
+    }
+
+    pub fn load(file: &mut File) -> std::result::Result<Settings, regex::Error> {
+        let mut buf = String::new();
+        file.read_to_string(&mut buf).expect("Failed to read file.");
+        let deser: SerializableSettings =
+            ron::de::from_str(buf.as_str()).expect("Failed to read file.");
+        Settings::try_from(deser)
     }
 }
 
-#[derive(Serialize, Debug)]
+impl TryFrom<SerializableSettings> for Settings {
+    type Error = regex::Error;
+    fn try_from(item: SerializableSettings) -> Result<Self, Self::Error> {
+        let ignore = item
+            .ignore
+            .iter()
+            .map(|x| Regex::from_str(x))
+            .collect::<Result<Vec<_>, _>>()?;
+        Ok(Settings {
+            ignore,
+            key_binds: item.key_binds,
+        })
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug)]
 struct SerializableSettings {
     ignore: Vec<String>,
     key_binds: KeyBinds,
 }
 
-impl TryFrom<Settings> for SerializableSettings {
-    type Error = &'static str;
-
-    fn try_from(value: Settings) -> Result<Self, Self::Error> {
-        Ok(SerializableSettings {
+impl From<Settings> for SerializableSettings {
+    fn from(value: Settings) -> Self {
+        SerializableSettings {
             ignore: value.ignore.iter().map(|x| x.to_string()).collect(),
             key_binds: value.key_binds,
-        })
+        }
     }
 }
