852b17f9dcac6ce4a74af2a842ae0deea866d30d
Grant Horner <h.grant.horner@gmail.com>
2020-06-26 12:12:12 -0400

    Found out that the project cannot work as planned. Rust (or any program) cannot invoke cd in the "housing" shell. It may be possible to feed the cd command to the external process.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,99 @@
 mod models;
 
 use models::{BookmarkCli, Cache, CacheRecord, Settings};
-use ron::ser::{to_string_pretty, PrettyConfig};
-use std::cmp::max;
+use ron::ser::{to_string, to_string_pretty, PrettyConfig};
+use std::convert::TryFrom;
+use std::env;
 use std::fs::{create_dir, File, OpenOptions};
+use std::io;
 use std::io::Write;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
 fn main() -> std::io::Result<()> {
     let args = BookmarkCli::from_args();
-    println!("{:?}", args);
-    let (cache, settings) = load_cache_and_settings();
-    println!("{:?}", settings);
-    println!("{:?}", cache);
+    let (mut cache, _settings) = load_cache_and_settings();
     match args {
         BookmarkCli::List => {
-            println!("Available bookmarks:");
-            let (len_name, len_description, len_path) =
-                cache.get_records().iter().fold((7, 14, 7), |acc, rec| {
-                    (
-                        max(acc.0, rec.name.len()),
-                        max(acc.1, rec.description.len()),
-                        max(acc.2, rec.path.to_str().unwrap_or("").len()),
-                    )
-                });
-            print_fixed_width("Name:", len_name);
-            print!(" | ");
-            print_fixed_width("Description:", len_description);
-            print!(" | ");
-            print_fixed_width("Path:", len_path);
-            print!("\n");
-            println!("{}", "-".repeat(len_name + len_description + len_path + 6));
+            cache.list_bookmarks();
         }
-        BookmarkCli::Add => {}
+        BookmarkCli::Add => {
+            let mut name = String::from("");
+            let mut description = String::from("");
+            let mut path = String::from("");
+            let path_buf: PathBuf;
+
+            match std::env::current_dir() {
+                Ok(dir) => {
+                    let s = dir.to_str().unwrap_or("").split('/').last().unwrap_or("");
+                    print!("Name [{:?}]: ", s);
+                    io::stdout().flush()?;
+                    io::stdin().read_line(&mut name)?;
+                    if name == "\n" {
+                        name = s.to_string();
+                    }
+                    print!("Description []: ");
+                    io::stdout().flush()?;
+                    io::stdin().read_line(&mut description)?;
+                    if description == "\n" {
+                        description = "".to_string();
+                    }
+                    print!("Path [{:?}]: ", dir);
+                    io::stdout().flush()?;
+                    io::stdin().read_line(&mut path)?;
+                    if path == "\n" {
+                        path_buf = dir;
+                    } else {
+                        path_buf = PathBuf::try_from(path).expect("Invalid path.");
+                    }
+
+                    cache.get_mut_records().push(CacheRecord {
+                        name: name.replace("\n", ""),
+                        description: description.replace("\n", ""),
+                        path: path_buf,
+                    });
+
+                    println!("{:?}", cache);
+                    save_cache_to_file(&cache);
+                }
+                Err(_) => {
+                    print!("Name: ");
+                }
+            }
+        }
         BookmarkCli::Remove => {}
+        BookmarkCli::Go { target } => {
+            let record = cache
+                .get_records()
+                .iter()
+                .find(|x| x.name == target)
+                .expect("Could not find matching name.");
+            env::set_current_dir(&record.path).expect("Failed to set current directory.");
+        }
     };
     Ok(())
+}
+
+fn save_cache_to_file(cache: &Cache) {
+    let mut cache_file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(
+            dirs::home_dir()
+                .expect("Could not find home directory.")
+                .join(".bkmk_config")
+                .join("cache.ron"),
+        )
+        .expect("Could not open cache file with write access.");
+    // let pretty = PrettyConfig::new()
+    //     .with_depth_limit(2)
+    //     .with_separate_tuple_members(true)
+    //     .with_enumerate_arrays(true);
+
+    let ser = to_string(cache.get_records()).expect("Serialization failed.");
+    cache_file
+        .write(ser.as_bytes())
+        .expect("Failed to write to cache file.");
 }
 
 fn load_cache_and_settings() -> (Cache, Settings) {
@@ -64,38 +121,61 @@ fn load_cache_and_settings() -> (Cache, Settings) {
 
     let settings =
         Settings::load_from_file(&mut settings_file).expect("Could not load settings file.");
-    let cache = Cache::load_from_file(&mut cache_file).expect("Could not load cache file.");
+    let cache = Cache::load_from_file(&mut cache_file);
     (cache, settings)
 }
 
 fn initialize_files(path: &PathBuf) -> (File, File) {
+    // I would like this to work by creating the file, writing to it,
+    // and then accessing the data in the file later (deserializing it).
+    // Unfortunately it seems like this doesn't work, and we can't access
+    // the newly created file. The current work around is to create the file,
+    // write to it, close it, then re-open it.
     create_dir(path).expect("Could not create a config directory.");
-    let mut cache_file =
-        File::create(path.join("cache.ron")).expect("Could not create a cache file.");
-    let cache: Vec<CacheRecord> = Vec::new();
-    let pretty = PrettyConfig::new()
-        .with_depth_limit(2)
-        .with_separate_tuple_members(true)
-        .with_enumerate_arrays(true);
-    let data = to_string_pretty(&cache, pretty).expect("Serialization failed.");
+    {
+        let mut cache_file = open_cache_file(path);
+        let cache: Vec<CacheRecord> = Vec::new();
+        let pretty = PrettyConfig::new()
+            .with_depth_limit(2)
+            .with_separate_tuple_members(true)
+            .with_enumerate_arrays(true);
+        let data = to_string_pretty(&cache, pretty).expect("Serialization failed.");
 
-    cache_file
-        .write(data.as_bytes())
-        .expect("Could not write to cache file.");
+        cache_file
+            .write(data.as_bytes())
+            .expect("Could not write to cache file.");
+        cache_file.flush().expect("Failed to flush cache file.");
 
-    let settings = models::Settings::new();
-    let mut settings_file =
-        File::create(path.join("settings.ron")).expect("Could not create a cache file.");
-    settings_file
-        .write(settings.serialize().as_bytes())
-        .expect("Could not write to settings file.");
+        let settings = models::Settings::new();
+        let mut settings_file = open_settings_file(path);
+
+        settings_file
+            .write(settings.serialize().as_bytes())
+            .expect("Could not write to settings file.");
+        settings_file
+            .flush()
+            .expect("Failed to flush settings file.");
+    }
+
+    let cache_file = open_cache_file(path);
+    let settings_file = open_settings_file(path);
     (cache_file, settings_file)
 }
 
-fn print_fixed_width(s: &str, n: usize) {
-    if s.len() < n {
-        print!("{}{}", s, " ".repeat(n - s.len()));
-    } else {
-        print!("{}", s);
-    }
+fn open_cache_file(path: &PathBuf) -> File {
+    OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path.join("cache.ron"))
+        .expect("Could not create a cache file.")
+}
+
+fn open_settings_file(path: &PathBuf) -> File {
+    OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path.join("settings.ron"))
+        .expect("Could not create a settings file.")
 }
--- src/models.rs
+++ src/models.rs
@@ -2,9 +2,11 @@ use core::str::FromStr;
 use regex::Regex;
 use ron::ser::{to_string_pretty, PrettyConfig};
 use serde::{Deserialize, Serialize};
+use std::cmp::max;
 use std::convert::{From, TryFrom};
 use std::fs::File;
 use std::io::Read;
+use std::io::Write;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -16,6 +18,11 @@ pub enum BookmarkCli {
     Add,
     /// Remove a bookmark
     Remove,
+    /// Go to the bookmarked directory
+    Go {
+        #[structopt(short, long)]
+        target: String,
+    },
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -29,16 +36,49 @@ impl Cache {
     pub fn get_records(&self) -> &Vec<CacheRecord> {
         &self.0
     }
+    pub fn get_mut_records(&mut self) -> &mut Vec<CacheRecord> {
+        &mut self.0
+    }
 
-    pub fn load_from_file(file: &mut File) -> std::result::Result<Cache, ron::Error> {
+    pub fn load_from_file(file: &mut File) -> Cache {
         let mut buf = String::new();
         file.read_to_string(&mut buf)
             .expect("Failed to read file to string.");
         if buf == "[]" {
-            Ok(Cache::new())
+            Cache::new()
         } else {
-            ron::de::from_str(buf.as_str())
+            Cache(
+                ron::de::from_str(buf.as_str())
+                    .expect("Could not convert string to Vec<CacheRecord>."),
+            )
         }
+    }
+    pub fn list_bookmarks(&self) {
+        println!("Available bookmarks:");
+        let (len_name, len_description, len_path) =
+            self.get_records().iter().fold((7, 14, 7), |acc, rec| {
+                (
+                    max(acc.0, rec.name.len()),
+                    max(acc.1, rec.description.len()),
+                    max(acc.2, rec.path.to_str().unwrap_or("").len()),
+                )
+            });
+        println!("{}", "-".repeat(len_name + len_description + len_path + 6));
+        print_fixed_width("Name:", len_name);
+        print!(" | ");
+        print_fixed_width("Description:", len_description);
+        print!(" | ");
+        print_fixed_width("Path:", len_path);
+        print!("\n");
+        println!("{}", "-".repeat(len_name + len_description + len_path + 6));
+        self.get_records().iter().for_each(|r| {
+            print_fixed_width(&r.name, len_name);
+            print!(" | ");
+            print_fixed_width(&r.description, len_description);
+            print!(" | ");
+            print_fixed_width(&r.path.to_str().unwrap_or(""), len_path);
+            print!("\n");
+        })
     }
 }
 
@@ -81,6 +121,9 @@ impl Settings {
 
     pub fn load_from_file(file: &mut File) -> std::result::Result<Settings, regex::Error> {
         let mut buf = String::new();
+        file.flush()
+            .expect("Failed to flush file when reading to string.");
+        file.sync_all().expect("Failed to sync data with the disk.");
         file.read_to_string(&mut buf)
             .expect("Failed to read file to string.");
         let deser: SerializableSettings =
@@ -116,5 +159,13 @@ impl From<Settings> for SerializableSettings {
             ignore: value.ignore.iter().map(|x| x.to_string()).collect(),
             key_binds: value.key_binds,
         }
+    }
+}
+
+fn print_fixed_width(s: &str, n: usize) {
+    if s.len() < n {
+        print!("{}{}", s, " ".repeat(n - s.len()));
+    } else {
+        print!("{}", s);
     }
 }
